8fec71a3e4f847dffca07a153588a4248ae42705
kevinbaschung <Focus1989*01>
2022-05-16 19:21:01 +0200

    added todo-item-container html and css

--- source/public/scripts/index.js
+++ source/public/scripts/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+
+const themeButton = document.getElementById("theme-button")
+const addTodoButton = document.getElementById("addTodo-button")
+const closeAddTodoButton = document.getElementById('close-addTodo-button')
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme")
+})
+
+addTodoButton.addEventListener("click", () => {
+  document.getElementById("todo-input-section").style.height = "12rem"
+})
+
+closeAddTodoButton.addEventListener("click", () => {
+  document.getElementById("todo-input-section").style.height= "0"
+})
